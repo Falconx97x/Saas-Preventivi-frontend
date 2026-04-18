fd7e129583dca50fdeba7c39cbba0c2a6f33166d
Falconx97x <mircobld2@gmail.com>
2026-04-18 20:20:19 +0200

    sec

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
-        <button style={{color: "black", border: "1px solid black", 
-                padding: "10px", borderRadius: "4px"}}
 
-                onClick={() => window.location.href = "/register"}>Registrati
-        </button>
+  useEffect(() => {
+    window.location.href = "/dashboard";
+   }, []);
 
-        <button style={{color: "black", border: "1px solid black", 
-                padding: "10px", borderRadius: "4px"}}
-
-                onClick={() => window.location.href = "/login"}>Accedi
-        </button>
-        
-      </main>
-    </div>
-  );
+  return <p> Redirect...</p>;
 }
